5901633f498a78b38e9f1a6fd7e5ca2512b788a1
Andrew Szot <me@andrewszot.com>
2017-02-02 17:58:56 -0800

    Added ability to load?

--- cifar_cnn.py
+++ cifar_cnn.py
@@ -4,6 +4,7 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 from helper.plot_helper import *
+from keras.models import load_model
 
 batch_size = 32
 classes_count = 10
@@ -13,55 +14,64 @@ input_shape = (32, 32)
 # RGB
 num_channels = 3
 
-(all_train_x, all_train_y), (all_test_x, all_test_y) = cifar10.load_data()
 
-def show_data():
-    show_cifar(all_train_x[0])
+def build_network():
+    (all_train_x, all_train_y), (all_test_x, all_test_y) = cifar10.load_data()
 
-all_train_y = np_utils.to_categorical(all_train_y, classes_count)
-all_test_y = np_utils.to_categorical(all_test_y, classes_count)
+    def show_data():
+        show_cifar(all_train_x[0])
 
-model = Sequential()
+    all_train_y = np_utils.to_categorical(all_train_y, classes_count)
+    all_test_y = np_utils.to_categorical(all_test_y, classes_count)
 
-model.add(Convolution2D(32, 3, 3, border_mode='same',
-    input_shape=all_train_x.shape[1:]))
-model.add(Activation('relu'))
-model.add(Convolution2D(32, 3, 3))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(0.25))
+    model = Sequential()
 
-model.add(Convolution2D(64, 3, 3, border_mode='same'))
-model.add(Activation('relu'))
-model.add(Convolution2D(64, 3, 3))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(0.25))
+    model.add(Convolution2D(32, 3, 3, border_mode='same',
+        input_shape=all_train_x.shape[1:]))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(32, 3, 3))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
 
-model.add(Flatten())
+    model.add(Convolution2D(64, 3, 3, border_mode='same'))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64, 3, 3))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.25))
 
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(classes_count))
-model.add(Activation('softmax'))
+    model.add(Flatten())
 
-model.compile(loss='categorical_crossentropy', optimizer='rmsprop',
-        metrics=['accuracy'])
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(classes_count))
+    model.add(Activation('softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop',
+            metrics=['accuracy'])
 
 
-all_train_x = all_train_x.astype('float32')
-all_test_x = all_test_x.astype('float32')
+    all_train_x = all_train_x.astype('float32')
+    all_test_x = all_test_x.astype('float32')
 
-all_train_x /= 255
-all_test_x /= 255
+    all_train_x /= 255
+    all_test_x /= 255
 
-model.fit(all_train_x, all_train_y, batch_size=batch_size,
-        nb_epoch=epoch_count, verbose=1)
+    model.fit(all_train_x, all_train_y, batch_size=batch_size,
+            nb_epoch=epoch_count, verbose=1)
 
-score = model.evaluate(all_test_x, all_test_y, verbose=1)
-print 'Accuracy: %.2f%%' % (score[1])
+    score = model.evaluate(all_test_x, all_test_y, verbose=1)
+    print 'Accuracy: %.2f%%' % (score[1])
 
-model.save('models/cifar_cnn.h5')
-print 'Model was saved'
+    model.save('models/cifar_cnn.h5')
+    print 'Model was saved'
 
+
+def load_network():
+    model = load_model('models/cifar_cnn.h5')
+    model.evaluate(all_test_x, all_test_y, verbose=1)
+
+
+load_network()
